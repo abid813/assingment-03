7d95020007a498ef3cf24f5ad3f5d56948cca15d
Abidul <abiduleislam@example.com>
2025-08-04 08:48:09 -0700

    Problem-02 pushed

--- assingment-03.js
+++ assingment-03.js
@@ -4,3 +4,15 @@ var area = 800;
 var sharableAsset = area/2;
 console.log(sharableAsset);
 
+
+// problem-02 (Cycle or Laptop)
+
+var money = 10000;
+if( money >= 25000){
+    console.log('Laptop');
+} else if (money >= 10000){
+    console.log('Cycle');
+}else{
+    console.log('chocolate');
+}
+
